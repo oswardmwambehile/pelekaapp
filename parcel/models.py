0661abfcfeb3f0a8fb94d26fe3a3448a00900159
oswardmwambehile <oswardmwambehile@gmail.com>
2025-07-09 14:56:31 +0300

    model

--- parcel/models.py
+++ parcel/models.py
@@ -16,25 +16,3 @@ class Parcel(models.Model):
 
     def __str__(self):
         return self.name
-
-from rest_framework import serializers
-from .models import Parcel
-
-class ParcelSerializer(serializers.ModelSerializer):
-    sender_email = serializers.EmailField(source='sender.email', read_only=True)
-    sender_username = serializers.CharField(source='sender.username', read_only=True)
-    sender_phone = serializers.CharField(source='sender.phone_number', read_only=True)
-    sender_profile_picture = serializers.ImageField(source='sender.profile_picture', read_only=True)
-    
-    # ✅ Add this field to show the computed price
-    price = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Parcel
-        fields = '__all__'  # includes all model fields + extra fields
-
-    def get_price(self, obj):
-        # Example pricing logic: base + per kg
-        base_price = 3000
-        rate_per_kg = 1000
-        return base_price + (obj.weight * rate_per_kg)
